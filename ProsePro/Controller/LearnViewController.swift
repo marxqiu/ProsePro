6d51a8003a3f4f4f7b02a667b7b8155ec75eed1d
marxqiu <marxqiu01@icloud.com>
2023-08-04 11:43:24 +0800

    Complete Segue

---
+++ ProsePro/Controller/LearnViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LearnViewController.swift
+//  ProsePro
+//
+//  Created by 裘渝琛 on 8/4/23.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
